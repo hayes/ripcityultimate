d8e674dbbfb82f40df66bf5cda9b42c2d0320f46
Michael Hayes <mhayes@newrelic.com>
2015-03-10 23:29:12 -0700

    create table

--- db.js
+++ db.js
@@ -2,3 +2,7 @@ var azure = require('azure-storage')
 var env = require('./env')
 
 module.exports = azure.createTableService(env.STORAGE_ACCOUNT, env.STORAGE_KEY)
+
+module.exports.createTableIfNotExists('users', function created(err) {
+  if (err) throw err
+})
